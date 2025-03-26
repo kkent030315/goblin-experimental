b096dc421987d0530c54668eff0f2ab52f1b5f96
kkent030315 <hrn832@protonmail.com>
2025-03-26 14:07:02 +0900

    PE: Add parser for .NET CLR
    https://github.com/m4b/goblin/pull/432

--- src/pe/mod.rs
+++ src/pe/mod.rs
@@ -13,6 +13,7 @@ use log::warn;
 pub mod authenticode;
 pub mod certificate_table;
 pub mod characteristic;
+pub mod clr;
 pub mod data_directories;
 pub mod debug;
 pub mod dll_characteristic;
@@ -81,6 +82,8 @@ pub struct PE<'a> {
     pub relocation_data: Option<relocation::RelocationData<'a>>,
     /// Certificates present, if any, described by the Certificate Table
     pub certificates: certificate_table::CertificateDirectoryTable<'a>,
+    /// CLR managed data if present
+    pub clr_data: Option<clr::ClrData<'a>>,
 }
 
 impl<'a> PE<'a> {
@@ -116,6 +119,7 @@ impl<'a> PE<'a> {
         let mut exception_data = None;
         let mut relocation_data = None;
         let mut certificates = Default::default();
+        let mut clr_data = Default::default();
         let mut is_64 = false;
         if let Some(optional_header) = header.optional_header {
             // Sections we are assembling through the parsing, eventually, it will be passed
@@ -265,6 +269,19 @@ impl<'a> PE<'a> {
                 )?);
             }
 
+            if let Some(com_descriptor) = optional_header.data_directories.get_clr_runtime_header()
+            {
+                let data = clr::ClrData::parse_with_opts(
+                    bytes,
+                    &com_descriptor,
+                    &sections,
+                    file_alignment,
+                    opts,
+                )?;
+                clr_data = Some(data);
+                debug!("CLR data: {:#?}", data);
+            }
+
             // Parse attribute certificates unless opted out of
             let certificate_table_size = if opts.parse_attribute_certificates {
                 if let Some(&certificate_table) =
@@ -320,6 +337,7 @@ impl<'a> PE<'a> {
             exception_data,
             relocation_data,
             certificates,
+            clr_data,
         })
     }
 
